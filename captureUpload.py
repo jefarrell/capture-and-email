36ef9db721bf4120f83102b2307179a6531f31e7
John Farrell <johnfarrell@john-farrells-MacBook-Pro.local>
2015-03-30 19:38:24 -0400

    initial commit

---
+++ captureUpload.py
@@ -0,0 +1,40 @@
+from subprocess import call
+import time
+import smtplib
+import pyscreenshot as ImageGrab
+
+
+####  GMAIL SMTP LOGIN  ####
+GMAIL_USERNAME = "cybernetiquethesis@gmail.com"
+GMAIL_PWD = "thesis666"
+recipients = "jf2813@nyu.edu"
+email_subject = "testing"
+body_of_email = "did the body work?"
+
+session = smtplib.SMTP('smtp.gmail.com', 587)
+session.ehlo()
+session.starttls()
+session.login(GMAIL_USERNAME, GMAIL_PWD)
+
+
+####  TAKE SCREENSHOTS  ####
+def screenshot():
+	for i in range(10):
+		print(i)
+		call(['screencapture','-x','test',str(i),'.png'])
+		#ImageGrab.grab_to_file('img'+str(i)+'.png')
+		time.sleep(5)
+
+while True:
+	screenshot()
+
+
+####  COMPILE AND SEND EMAIL  ####
+headers = "\r\n".join(["from: " + GMAIL_USERNAME,
+	"subject: " + email_subject,
+	"to : " + recipients,
+	"mime-version: 1.0",
+	"content-type: text/html"])
+
+content = headers + "\r\n\r\n" + body_of_email
+#session.sendmail(GMAIL_USERNAME, recipients, content)
