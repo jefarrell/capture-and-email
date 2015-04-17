39fa122c5e0ab8383d87d61861d2eef18e675e53
John Farrell <johnfarrell@172-16-248-228.DYNAPOOL.NYU.EDU>
2015-04-17 15:48:30 -0400

    email attachment

--- python/captureUpload.py
+++ python/captureUpload.py
@@ -1,17 +1,13 @@
+import smtplib, os, email, re, sys, glob, string, time, datetime
 from subprocess import call
 import subprocess as sp
-import time
-import smtplib
-import pyscreenshot as ImageGrab
-import assets
-import os
 from email.mime.multipart import MIMEMultipart
 from email.mime.text import MIMEText
 from email.MIMEBase import MIMEBase
 from email import Encoders
+import assets
+
 FFMPEG_BIN = "ffmpeg"
-
-
 
 command = [ FFMPEG_BIN,
 '-framerate',
@@ -47,39 +43,18 @@ screenshot()
 
 
 
-
-####  GMAIL SMTP LOGIN  ####
-GMAIL_USERNAME = assets.user
-GMAIL_PWD = assets.pwd
-recipients = assets.recipients
-email_subject = "testing"
-body_of_email = "did the body work?"
-file_path = 'out.mp4'
-
-msg = MIMEMultipart()
-msg.attach(MIMEText(body_of_email))
-part = MIMEBase('application', "octet-stream")
-fo = open(file_path,'rb')
-part.set_payload(fo.read())
-Encoders.encode_base64(part)
-part.add_header('Content-Disposition', 'attachment; filename="%s"' % os.path.basename(file))
-msg.attach(part)
-
-mailer = smtplib.SMTP('smtp.gmail.com', 587)
-mailer.ehlo()
-mailer.starttls()
-mailer.login(GMAIL_USERNAME,GMAIL_PWD)
-sent = mailer.sendmail(GMAIL_USERNAME, recipients, msg.as_string())
-mailer.close
-
-
-
-
+####  GMAIL WORKING  ####
+# GMAIL_USERNAME = assets.user
+# GMAIL_PWD = assets.pwd
+# recipients = assets.recipients
+# email_subject = "test"
+# body_of_email = "working"
 
 # session = smtplib.SMTP('smtp.gmail.com', 587)
 # session.ehlo()
 # session.starttls()
 # session.login(GMAIL_USERNAME, GMAIL_PWD)
+# print("logged in")
 
 # ####  COMPILE AND SEND EMAIL  ####
 # headers = "\r\n".join(["from: " + GMAIL_USERNAME,
@@ -90,11 +65,67 @@ mailer.close
 # 	"content-type: text/html"])
 
 # content = headers + "\r\n\r\n" + body_of_email
+# print("compiled")
 # session.sendmail(GMAIL_USERNAME, recipients, content)
+# print("sent")
 
 
 
 
+attachmentname = 'h92yr.gif'
+username = assets.user
+password = assets.pwd
+fromaddr = assets.user
+toaddr  = assets.recipients
+
+
+msgsubject = 'Here is another subject'
+
+htmlmsgtext = """EMAIL BODY in HTML<br/>
+                Yup. Yup. Yup.</br>
+                <b>Done!</a>"""
+
+try:
+
+    msgtext = htmlmsgtext.replace('<b>','').replace('</b>','').replace('<br>',"\r").replace('</br>',"\r").replace('<br/>',"\r").replace('</a>','')
+    msgtext = re.sub('<.*?>','',msgtext)
+
+    msg = MIMEMultipart()
+    msg.preamble = 'This is a multi-part message in MIME format.\n'
+    msg.epilogue = ''
+
+    body = MIMEMultipart('alternative')
+    body.attach(MIMEText(msgtext))
+    body.attach(MIMEText(htmlmsgtext, 'html'))
+    msg.attach(body)
+
+    # Is there an attachment?
+    if 'attachmentname' in globals(): 
+        f = attachmentname
+        part = MIMEBase('application', "octet-stream")
+        part.set_payload( open(f,"rb").read() )
+        Encoders.encode_base64(part)
+        part.add_header('Content-Disposition', 'attachment; filename="%s"' % os.path.basename(f))
+        msg.attach(part)
+
+    msg.add_header('From', fromaddr)
+    msg.add_header('To', toaddr)
+    msg.add_header('Subject', msgsubject)
+
+    # Login and send
+    server = smtplib.SMTP('smtp.gmail.com:587')
+    server.set_debuglevel(True) 
+    server.starttls()
+    server.login(username,password)
+    server.sendmail(msg['From'], [msg['To']], msg.as_string())
+
+    server.quit() 
+
+
+
+
+except:
+    print ('Email NOT sent to %s successfully. %s ERR: %s %s %s ', str(toaddr), 'tete', str(sys.exc_info()[0]), str(sys.exc_info()[1]), str(sys.exc_info()[2]) )
 
 
 
